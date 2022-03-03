7eb8d5cbc040668cca9120e659e8a021c3ee92e7
gggyy3 <1587644910@qq.com>
2022-03-03 17:36:44 +0800

    add header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,8 @@ import headerBg from '../images/header.png'
 
 export const Home = () => {
   return (
-    <div>
-      <div className="x" style={{backgroundImage: `url(${headerBg})`}}></div>
+    <div className='home'>
+      <header style={{backgroundImage: `url(${headerBg})`}} />
     </div>
   );
 };
