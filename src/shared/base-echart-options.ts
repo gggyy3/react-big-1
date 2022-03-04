c4b6ec3522110e9c342b18228ac25a82311a2f40
gggyy3 <1587644910@qq.com>
2022-03-04 11:13:08 +0800

    修改 grid

--- src/shared/base-echart-options.ts
+++ src/shared/base-echart-options.ts
@@ -1,16 +1,17 @@
 import { px } from "./px";
 
 export const baseEchartOptions = {
-    textStyle: {
-        fontSize: px(12),
-        color: "#79839e",
-      },
-      title: { show: false },
-    legend: { show: false },
-    grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40),
-      }
-}
+  textStyle: {
+    fontSize: px(12),
+    color: "#79839e",
+  },
+  title: { show: false },
+  legend: { show: false },
+  grid: {
+    x: px(20),
+    y: px(20),
+    x2: px(20),
+    y2: px(20),
+    containLabel: true,
+  },
+};
