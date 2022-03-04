0cbcb4c07adb40034e3c6bd668206cc435e25c19
gggyy3 <1587644910@qq.com>
2022-03-04 11:22:13 +0800

    制作 Chart3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,9 +3,10 @@ import headerBg from "../images/header.png";
 import React, { useEffect, useRef } from "react";
 import { Chart1 } from "../components/chart-1";
 import { Chart2 } from "../components/chart-2";
+import { Chart3 } from "../components/chart-3";
 
 export const Home = () => {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
   return (
     <div className="home">
       <header style={{ backgroundImage: `url(${headerBg})` }} />
@@ -14,15 +15,14 @@ export const Home = () => {
           <Chart1 />
           <Chart2 />
         </section>
-        <section className="bordered section2">
+        <section className="section2">
+          <Chart3 />
         </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
-      <footer>
-        &copy;gggy 2022-{ year}
-      </footer>
+      <footer>&copy;gggy 2022-{year}</footer>
     </div>
   );
 };
