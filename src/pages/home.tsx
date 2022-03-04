3cf33e666489b267ae625169a5c1e7f8e2759e67
gggyy3 <1587644910@qq.com>
2022-03-04 10:38:41 +0800

    重构提取出 create-echarts-options.ts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
 import "./home.scss";
 import headerBg from "../images/header.png";
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from "react";
 import { Chart1 } from "../components/chart-1";
+import { Chart2 } from "../components/chart-2";
 
 export const Home = () => {
   return (
@@ -10,8 +11,10 @@ export const Home = () => {
       <main>
         <section className="section1">
           <Chart1 />
+          <Chart2 />
         </section>
-        <section className="bordered section2"></section>
+        <section className="bordered section2">
+        </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
